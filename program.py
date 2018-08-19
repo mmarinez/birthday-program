7adaa182e93272d7842135ff551e352c2973edc4
Manuel Marinez <marinezdev@gmail.com>
2018-08-19 12:05:34 -0400

    Add date format to the user input

--- program.py
+++ program.py
@@ -1,3 +1,15 @@
-year = input('Enter your birth year: ')
-month = input('Enter your birth month: ')
-day = input('Enter yout birth day: ')
+import datetime
+
+def get_user_date_input():
+    year = int(input('Enter your birth year: '))
+    month = int(input('Enter your birth month: '))
+    day = int(input('Enter yout birth day: '))
+
+    birthday = datetime.date(year, month, day)
+    return birthday
+
+def main():
+    user_birthday = get_user_date_input()
+    print(user_birthday)
+
+main()
